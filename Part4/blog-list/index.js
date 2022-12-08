c87d02d6152a42f79fdd0b1fc788848c7980fc4c
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-08 17:59:04 +0200

    ✅ Completed Exercises 4.1-4.2

--- Part4/blog-list/index.js
+++ Part4/blog-list/index.js
@@ -1,39 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const http = require("http");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const server = http.createServer(app);
 
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URL;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
--- Part4/blog-list/utils/config.js
+++ Part4/blog-list/utils/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = 3003;
 
 module.exports = {
   MONGODB_URL,
