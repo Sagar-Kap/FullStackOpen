e9578f80040d711daa1065ecbd7234869194cc15
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-30 14:28:41 +0200

    ✅ Completed Exercise 5.7

--- Part5/bloglist/src/components/Blog.jsx
+++ Part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,24 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div>
+      {blog.title} - {blog.author}{" "}
+      <button onClick={onClick}>{visible ? "hide" : "show"}</button>
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>{blog.likes}</div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
