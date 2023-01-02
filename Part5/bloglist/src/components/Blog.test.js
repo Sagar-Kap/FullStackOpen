37e45bc0ae9bc756cf4be6df47da9383a4b42bab
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-02 16:35:39 +0200

    ✅ Completed Exercise 5.15

--- Part5/bloglist/src/components/Blog.test.js
+++ Part5/bloglist/src/components/Blog.test.js
@@ -5,51 +5,85 @@ import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
 let component;
 
-test("Renders blog on the screen", () => {
-  const blog = {
-    title: "Component gets rendered with this test",
-    author: "Test1",
-    url: "test.com/test1",
-    likes: 787,
-    user: {
+describe("Renders blog on the screen", () => {
+  test("Renders blog on the screen", () => {
+    const blog = {
+      title: "Component gets rendered with this test",
+      author: "Test1",
+      url: "test.com/test1",
+      likes: 787,
+      user: {
+        username: "jhing",
+      },
       username: "jhing",
-    },
-    username: "jhing",
-  };
+    };
 
-  component = render(<Blog blog={blog} />);
+    component = render(<Blog blog={blog} />);
 
-  expect(component.container.querySelector(".blog")).toHaveTextContent(
-    "Component gets rendered with this test"
-  );
-  expect(component.container.querySelector(".blog")).toHaveTextContent("Test1");
-  expect(component.container.querySelector(".blog")).not.toHaveTextContent(
-    "test.com/test1"
-  );
-  expect(component.container.querySelector(".blog")).not.toHaveTextContent(
-    "787"
-  );
+    expect(component.container.querySelector(".blog")).toHaveTextContent(
+      "Component gets rendered with this test"
+    );
+    expect(component.container.querySelector(".blog")).toHaveTextContent(
+      "Test1"
+    );
+    expect(component.container.querySelector(".blog")).not.toHaveTextContent(
+      "test.com/test1"
+    );
+    expect(component.container.querySelector(".blog")).not.toHaveTextContent(
+      "787"
+    );
+  });
 });
 
-test("Clicking the button shows other details", async () => {
-  const blog = {
-    title: "Component gets rendered with this test",
-    author: "Test1",
-    url: "test.com/test1",
-    likes: 787,
-    user: {
+describe("Clicking the button shows other details", () => {
+  test("Clicking the button shows other details", async () => {
+    const blog = {
+      title: "Component gets rendered with this test",
+      author: "Test1",
+      url: "test.com/test1",
+      likes: 787,
+      user: {
+        username: "jhing",
+      },
       username: "jhing",
-    },
-    username: "jhing",
-  };
+    };
 
-  component = render(<Blog blog={blog} />);
+    component = render(<Blog blog={blog} />);
 
-  const user = userEvent.setup();
-  const button = screen.getByText("Show");
-  await user.click(button);
-  expect(component.container.querySelector(".blog")).toHaveTextContent(
-    "test.com/test1"
-  );
-  expect(component.container.querySelector(".blog")).toHaveTextContent("787");
+    const user = userEvent.setup();
+    const button = screen.getByText("Show");
+    await user.click(button);
+    expect(component.container.querySelector(".blog")).toHaveTextContent(
+      "test.com/test1"
+    );
+    expect(component.container.querySelector(".blog")).toHaveTextContent("787");
+  });
+});
+
+describe("Check like button", () => {
+  test("Get two event calls when like button is clicked twice", async () => {
+    const blog = {
+      title: "Component gets rendered with this test",
+      author: "Test1",
+      url: "test.com/test1",
+      likes: 787,
+      user: {
+        username: "jhing",
+      },
+      username: "jhing",
+    };
+
+    const mockHandler = jest.fn();
+
+    component = render(<Blog blog={blog} updateLike={mockHandler} />);
+
+    const user = userEvent.setup();
+    let button = screen.getByText("Show");
+    await user.click(button);
+
+    button = screen.getByText("👍");
+    await user.click(button);
+    await user.click(button);
+    expect(mockHandler.mock.calls).toHaveLength(2);
+  });
 });
