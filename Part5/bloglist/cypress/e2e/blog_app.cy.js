0f4b7f2fbe84f0ef0adabd8d51e47aa009fe1379
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-03 15:34:09 +0200

    ✅ Completed Exercise 5.17

--- Part5/bloglist/cypress/e2e/blog_app.cy.js
+++ Part5/bloglist/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,10 @@
 describe("Blog app", function () {
-  it("front page can be opened", function () {
-    cy.visit("http://localhost:3000");
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("Login form is shown", function () {
     cy.contains("Log in to the application");
   });
 });
