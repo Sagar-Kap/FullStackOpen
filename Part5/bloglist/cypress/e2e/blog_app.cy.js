e21d42b2ecffa86b7888aa229a43e460be6e6158
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-03 08:44:38 +0200

    ✨ Configured Cypress ESlint

--- Part5/bloglist/cypress/e2e/blog_app.cy.js
+++ Part5/bloglist/cypress/e2e/blog_app.cy.js
@@ -1,4 +1,4 @@
-describe("Note app", function () {
+describe("Blog app", function () {
   it("front page can be opened", function () {
     cy.visit("http://localhost:3000");
     cy.contains("Log in to the application");
