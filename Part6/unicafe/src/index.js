88081683e4e143d6b790644dca930a78703a6534
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-11 09:33:29 +0200

    ♻️ Moved code into components

--- Part6/unicafe/src/index.js
+++ Part6/unicafe/src/index.js
@@ -1,34 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const store = createStore(reducer)
-
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
-  return (
-    <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
