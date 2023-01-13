6ac779cbe97fdd1f029a86297c1195decec50199
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-13 10:09:22 +0200

    ✅ COmpleted Exercise 6.1 and 6.2

--- Part6/unicafe/src/index.js
+++ Part6/unicafe/src/index.js
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createStore } from "redux";
+import reducer from "./reducer";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const store = createStore(reducer);
+
+const App = () => {
+  const handleClick = (type) => {
+    store.dispatch({ type });
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleClick("GOOD")}>good</button>
+      <button onClick={() => handleClick("OK")}>ok</button>
+      <button onClick={() => handleClick("BAD")}>bad</button>
+      <button onClick={() => handleClick("ZERO")}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
