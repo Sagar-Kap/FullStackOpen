8644a7140761746391a7993eaed33cbf80d6d8a9
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-17 08:42:12 +0200

    ✅ Completed Exercise 6.9

--- Part6/anecdotes/src/index.js
+++ Part6/anecdotes/src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import reducer from "./reducers/anecdoteReducer";
-
-const store = createStore(reducer);
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
