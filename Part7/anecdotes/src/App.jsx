39f5b272beb98708c6e8fd550ffe0dd154b3ef87
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-24 06:36:20 +0200

    ✅ Completed Exercise 7.1

--- Part7/anecdotes/src/App.jsx
+++ Part7/anecdotes/src/App.jsx
@@ -7,15 +7,15 @@ const Menu = () => {
   };
   return (
     <div>
-      <a href="#" style={padding}>
+      <Link style={padding} to="/">
         anecdotes
-      </a>
-      <a href="#" style={padding}>
+      </Link>
+      <Link style={padding} to="/create">
         create new
-      </a>
-      <a href="#" style={padding}>
+      </Link>
+      <Link style={padding} to="about">
         about
-      </a>
+      </Link>
     </div>
   );
 };
@@ -156,9 +156,13 @@ const App = () => {
       <div>
         <h1>Software anecdotes</h1>
         <Menu />
-        <AnecdoteList anecdotes={anecdotes} />
-        <About />
-        <CreateNew addNew={addNew} />
+
+        <Routes>
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+          <Route path="create" element={<CreateNew addNew={addNew} />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+
         <Footer />
       </div>
     </Router>
