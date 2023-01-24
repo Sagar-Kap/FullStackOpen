e66432150c7f7cf31d3578482d5db065b91afe6c
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-24 07:22:35 +0200

    ✅ Completed Exercise 7.2

--- Part7/anecdotes/src/App.jsx
+++ Part7/anecdotes/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
 const Menu = () => {
   const padding = {
@@ -20,12 +26,26 @@ const Menu = () => {
   );
 };
 
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id;
+  const anecdote = anecdotes.find((n) => n.id === Number(id));
+  return (
+    <div>
+      <h2>{`${anecdote.content} by ${anecdote.author}`}</h2>
+      <p>{`Has ${anecdote.votes} votes`}</p>
+      <p>{`For more info see ${anecdote.info}`}</p>
+    </div>
+  );
+};
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
@@ -158,6 +178,10 @@ const App = () => {
         <Menu />
 
         <Routes>
+          <Route
+            path="/anecdotes/:id"
+            element={<Anecdote anecdotes={anecdotes} />}
+          />
           <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
           <Route path="create" element={<CreateNew addNew={addNew} />} />
           <Route path="about" element={<About />} />
