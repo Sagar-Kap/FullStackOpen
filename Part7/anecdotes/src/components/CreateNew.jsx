9a06a558f8239647a47e82722e9e6e5de2792623
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-25 17:00:46 +0200

    ✅ Completed Exercise 7.4

--- Part7/anecdotes/src/components/CreateNew.jsx
+++ Part7/anecdotes/src/components/CreateNew.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
+
 const CreateNew = ({ addNew }) => {
   const navigate = useNavigate();
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate("/");
@@ -23,27 +24,15 @@ const CreateNew = ({ addNew }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
