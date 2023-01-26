5543f20866b8baee4812e625fab7c06adc3bae62
Sagar Kapoor <sagrkapr@gmail.com>
2023-01-26 10:06:27 +0200

    ✅ Completed Exercise 7.8

--- Part7/notes/src/App.js
+++ Part7/notes/src/App.js
@@ -1,74 +1,54 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
-
-  const service = {
-    create
-  }
-
-  return [
-    resources, service
-  ]
-}
+import { useField, useResource } from "./hooks";
+import { useEffect } from "react";
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const content = useField("text");
+  const name = useField("text");
+  const number = useField("text");
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const [notes, noteService] = useResource("http://localhost:3005/notes");
+  const [persons, personService] = useResource("http://localhost:3005/persons");
+
+  useEffect(() => {
+    noteService.getAll();
+    personService.getAll();
+    // eslint-disable-next-line
+  }, []);
 
   const handleNoteSubmit = (event) => {
-    event.preventDefault()
-    noteService.create({ content: content.value })
-  }
- 
+    event.preventDefault();
+    noteService.create({ content: content.value });
+  };
+
   const handlePersonSubmit = (event) => {
-    event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
-  }
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+  };
 
   return (
     <div>
-      <h2>notes</h2>
+      <h2>Notes</h2>
       <form onSubmit={handleNoteSubmit}>
         <input {...content} />
-        <button>create</button>
+        <button>Create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
-      <h2>persons</h2>
+      <h2>Persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
-        <button>create</button>
+        Name <input {...name} /> <br />
+        Number <input {...number} />
+        <button>Create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
