441e7dcc2ed911493d85f81a18d9eabab2373af1
Sagar Kapoor <sagrkapr@gmail.com>
2023-02-07 17:27:19 +0200

    ✅ Completed Exercise 8.4

--- Part8/library/index.js
+++ Part8/library/index.js
@@ -99,7 +99,7 @@ type Author {
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]
+    allBooks(author: String): [Book!]
     allAuthors: [Author!]
   }
 `;
@@ -108,7 +108,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books;
+      return books.filter((book) => book.author === args.author);
+    },
     allAuthors: () => {
       return authors.map((author) => {
         const bookCount = books.filter(
