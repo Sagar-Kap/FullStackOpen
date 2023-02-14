b5a7638b6f8b3769db8b7e37e8432e3ec03f7620
Sagar Kapoor <sagrkapr@gmail.com>
2023-02-14 09:10:31 +0200

    ♻️ Refractored library backend

---
+++ Part8/library/resolvers.js
@@ -0,0 +1,142 @@
+const { v1: uuid } = require("uuid");
+const { GraphQLError } = require("graphql");
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+];
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "The Demon ",
+    published: 1872,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+];
+
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+      let filteredBooks = books;
+      if (args.author) {
+        filteredBooks = filteredBooks.filter(
+          (book) => book.author === args.author
+        );
+      }
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((book) => {
+          return book.genres.includes(args.genre);
+        });
+      }
+      return filteredBooks;
+    },
+    allAuthors: () => {
+      return authors.map((author) => {
+        const bookCount = books.filter(
+          (book) => book.author === author.name
+        ).length;
+        return { ...author, bookCount };
+      });
+    },
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if (books.find((p) => p.title === args.title)) {
+        throw new GraphQLError("Title must be unique", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+          },
+        });
+      }
+
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      if (!authors.find((p) => p.name === args.author)) {
+        authors.push({ name: args.author, born: null, bookCount: 1 });
+      }
+      return book;
+    },
+
+    editAuthor: (root, args) => {
+      if (!authors.find((p) => p.name === args.name)) {
+        return null;
+      }
+
+      objIndex = authors.findIndex((obj) => obj.name === args.name);
+      const newAuthor = { ...authors[objIndex], born: args.setBornTo };
+      authors[objIndex] = newAuthor;
+      return newAuthor;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ Part8/library/schema.js
@@ -0,0 +1,38 @@
+const typeDefs = `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    id: ID!
+    genres: [String!]
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: String!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book]
+    allAuthors: [Author!]
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String]
+    ): Book
+
+    addAuthor(name: String!, born: Int, bookCount: String!): Author
+
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
+`;
+
+module.exports = typeDefs;
