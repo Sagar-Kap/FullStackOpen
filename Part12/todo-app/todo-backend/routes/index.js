c726e1db2abe6a7ba395a1672bf4076c08692841
Sagar Kapoor <sagrkapr@gmail.com>
2023-03-23 17:24:06 +0200

    ✅ Completed Exercise 12.b

--- Part12/todo-app/todo-backend/routes/index.js
+++ Part12/todo-app/todo-backend/routes/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { client, setAsync, getAsync } = require("../redis");
 
 const configs = require("../util/config");
 
-let visits = 0;
-
 /* GET index data. */
 router.get("/", async (req, res) => {
-  visits++;
+  let visits;
+
+  const sitevisits = await getAsync("sitecounter");
+  if (!sitevisits) {
+    client.set("sitecounter", "1");
+    visits = 1;
+  } else {
+    visits = parseInt(sitevisits) + 1;
+    client.set("sitecounter", visits.toString());
+  }
 
   res.send({
     ...configs,
